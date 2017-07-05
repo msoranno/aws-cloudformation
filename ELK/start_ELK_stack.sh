01f618388ad2b573fd8502b253dc981472e8587f
Miguel Soranno <msoranno@gmail.com>
2017-07-05 12:57:27 +0200

    se agregan scripts para manejar un poco el stack elk

---
+++ ELK/start_ELK_stack.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name stackELK --template-body file://VPC_Public_Ec2_Centos7_Docker-ELK.json --parameters ParameterKey=KeyName,ParameterValue=key_08062017 ParameterKey=SSHLocation,ParameterValue=0.0.0.0/0 ParameterKey=EC2InstanceType,ParameterValue=t2.large
